89d5fb4950b907bc089b6046a6b3e44817b19cdc
kimyena1123 <kyq104@gmail.com>
2024-03-02 21:27:31 +0900

    여러 환경 변수 파일: 서버 기동시 어떤 환경 변수 출력되는 지 확인

--- chapter9/config-test/src/app.module.ts
+++ chapter9/config-test/src/app.module.ts
@@ -5,6 +5,8 @@ import { AppService } from "./app.service";
 import { WeatherModule } from './weather/weather.module';
 import { WeatherController } from './weather/weather.controller';
 
+console.log('env: ' + process.env.NODE_ENV); //기동 시 환경 변수 출력
+
 @Module({
   imports: [ConfigModule.forRoot({isGlobal: true}), WeatherModule], //전역 모듈 설정 추가
   controllers: [AppController, WeatherController],
