67aef7e9b19907b936dca0bc29d268134fb1ce80
kimyena1123 <kyq104@gmail.com>
2024-03-02 14:30:57 +0900

    [chapter8_blog] BlogSerivce를 controller에서 사용할 수 있게 수정 & HTTP 요청을 처리할 수 있게 함

--- chapter8/blog/src/blog.controller.ts
+++ chapter8/blog/src/blog.controller.ts
@@ -1,31 +1,48 @@
 import {Controller, Param, Body, Delete, Get, Post, Put} from '@nestjs/common';
+import {BlogService} from './blog.service';
 
 @Controller('blog') //클래스에 붙이는 controller 데코레이터 => {서버주소}/blog 이하의 요청을 처리한다는 뜻.
 export class BlogController{
+
+    blogService: BlogService;
+
+    constructor(){
+        this.blogService = new BlogService(); //생성자에서 블로그 서비스 생성
+    }
+
     @Get() 
     getAllPosts(){
         console.log("모든 게시물 가져오기");
+        
+        return this.blogService.getAllPosts();
     }
 
     @Post()
-    createPost(@Body() post: any){ // HTTP 요청의 body 내용을 post에 할당
+    createPost(@Body() postDto){ // HTTP 요청의 body 내용을 post에 할당
         console.log("게시글 작성");
-        console.log(post);
+        this.blogService.createPost(postDto);
+
+        return 'success';
     }
 
     @Get('/:id')
     getPost(@Param('id') id: string){
         console.log(`[id: ${id}] 게시글 하나 가져오기`)
+
+        return this.blogService.getPost(id);
     }
 
     @Delete('/:id')
-    deletePost(){
+    deletePost(@Param('id') id: string){
         console.log("게시글 삭제");
+
+        return 'success';
     }
 
     @Put('/:id')
-    updatePost(@Param('id') id, @Body() post: any){
+    updatePost(@Param('id') id: string, @Body() postDto){
         console.log(`[${id}] 게시글 업데이트`);
-        console.log(post);
+
+        return this.blogService.updatePost(id, postDto);
     }
 }
