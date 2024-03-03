100375a880dc31c6ca59f377a57ad6e79b855d47
kimyena1123 <kyq104@gmail.com>
2024-03-03 17:35:02 +0900

    파이프로 유효성 검사하기: 전역 ValidationPipe 설정하기

--- chapter10/nest-auth-test/src/main.ts
+++ chapter10/nest-auth-test/src/main.ts
@@ -1,8 +1,11 @@
+import { ValidationPipe } from '@nestjs/common'; //validationPipe 임포트 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); //전역 파이프에 validationPipe 객체 추가
+
   await app.listen(3000);
 }
 bootstrap();
