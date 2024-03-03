2fa5760ee27653ac2da90b2b874bf1ec5c865ed5
kimyena1123 <kyq104@gmail.com>
2024-03-03 23:45:39 +0900

    back과 front 연동: backend 코드와 ejs의 파일을 연결

--- chapter10/nest-auth-test/src/app.controller.ts
+++ chapter10/nest-auth-test/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,11 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/register') //localhost:3000/register
+  @Render('register')
+  getShowView(){
+    return {title: "Register Page"};
+  }
+
 }
