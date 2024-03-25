02d5bc30743dbf32a5f59f530ba92bf4f4356abc
kimyena1123 <kyq104@gmail.com>
2024-03-25 23:30:49 +0900

    쿠키를 사용한 인증 구현(user 폴더는 암호화x, auth 폴더o)

--- chapter10/nest-auth-test/src/auth/auth.controller.ts
+++ chapter10/nest-auth-test/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, Response } from '@nestjs/common';
 import { CreateUserDto } from 'src/user/user.dto';
 import { AuthService } from './auth.service';
 
@@ -11,5 +11,23 @@ export class AuthController {
     //class-validator가 자동으로 유효성 검증
     async register(@Body() userDto: CreateUserDto){
         return await this.authService.register(userDto); //authService를 사용해 user 정보 저장
+    }
+
+    @Post('/login')
+    async login(@Request() req, @Response() res){
+        //validateUser를 호출해 유저 정보 획득
+        const userInfo = await this.authService.validateUser(
+            req.body.email,
+            req.body.password,
+        );
+
+        //유저 정보가 있으면, 쿠키 정보를 response에 저장
+        if(userInfo){
+            res.cookie('login', JSON.stringify(userInfo), {
+                httpOnly: false, //브라우저에서 읽을 수 있도록 한다.
+                maxAge: 1000 * 60 * 60 * 24 * 7. //7day 단위는 밀리초
+            });
+        }
+        return res.send({message: 'login success'});
     }
 }
--- chapter10/nest-auth-test/src/auth/auth.service.ts
+++ chapter10/nest-auth-test/src/auth/auth.service.ts
@@ -35,6 +35,25 @@ export class AuthService {
         }
     }
 
+    async validateUser(email:string, password:string){
+        //이메일로 유저 정보를 받아옴
+        const user = await this.userService.getUser(email);
+
+        if(!user){ //유저가 없으면 검증 실패
+            return null;
+        }
+        
+        //패스워드를 따로 뽑아냄
+        //패스워드를 검증해야 하는데 유저 정보에서 패스워드만 따로 뽑아서 hashedPassword라는 변수로 받아온다.
+        const {password: hashedPassword, ...userInfo} = user;
+        if(bcrypt.compareSync(password, hashedPassword)){ 
+            //bcrypt.compasreSync(data, encrypted) 함수에서 
+            //data는 입력받은 패스워드 값, 두번째에는 패스워드 해시값을 넣어주면, 바르게 암호화된 경우 userInfo를 반환한다. 아니라면 null을 반환한다.
+            return userInfo;
+        }
+        return null;
+    }
+
     
 }
 
